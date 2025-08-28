51ac454c3448b71335aa6dddf2f4fae0ec96c33b
Walder Andreas <andreas.walder@web.de>
2025-08-28 20:51:33 +0200

    Update __init__.py

--- custom_components/teleco_daisy/__init__.py
+++ custom_components/teleco_daisy/__init__.py
@@ -2,12 +2,12 @@ import logging
 
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_PASSWORD, CONF_USERNAME
-from homeassistant.core import HomeAssistant
+from homeassistant.core import HomeAssistant, ServiceCall
+
 from .const import DOMAIN
 from .hub import DaisyHub
 
 _LOGGER = logging.getLogger(__name__)
-
 PLATFORMS: list[str] = ["light", "cover"]
 
 
@@ -18,20 +18,13 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
     hass.data.setdefault(DOMAIN, {})[entry.entry_id] = daisy_hub
 
-    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
-    return True
-
-  async def handle_feed_command(call: ServiceCall):
+    async def handle_feed_command(call: ServiceCall):
         device_code = call.data["deviceCode"]
         id_device = call.data["idInstallationDevice"]
         command_action = call.data["commandAction"]
         command_id = call.data["commandId"]
         command_param = call.data["commandParam"]
         lowlevel = call.data.get("lowlevelCommand")
-
-        # Instanz des Clients (TelecoDaisy) und Installation holen:
-        client: TelecoDaisy = hass.data[DOMAIN]["client"]
-        installation: DaisyInstallation = hass.data[DOMAIN]["installation"]
 
         command = {
             "commandAction": command_action,
@@ -43,12 +36,11 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         if lowlevel:
             command["lowlevelCommand"] = lowlevel
 
-        await hass.async_add_executor_job(
-            lambda: client.feed_the_commands(installation, [command])
-        )
+        await hass.async_add_executor_job(daisy_hub.feed_command, command)
 
     hass.services.async_register(DOMAIN, "feed_command", handle_feed_command)
 
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
 
@@ -56,5 +48,4 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
     if unload_ok:
         hass.data[DOMAIN].pop(entry.entry_id)
-
     return unload_ok
