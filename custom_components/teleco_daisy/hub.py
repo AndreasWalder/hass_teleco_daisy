f80568ef6e6d11a5043569cb71f6eade50dc7753
Walder Andreas <andreas.walder@web.de>
2025-08-28 20:50:42 +0200

    Update hub.py

--- custom_components/teleco_daisy/hub.py
+++ custom_components/teleco_daisy/hub.py
@@ -7,6 +7,7 @@ from teleco_daisy import (
     DaisyRGBLight,
     DaisyAwningsCover,
     DaisySlatsCover,
+    DaisyInstallation,  # hinzufügen
 )
 
 
@@ -22,23 +23,31 @@ class DaisyHub(TelecoDaisy):
         self._name = "Teleco DaisyHub"
         self._id = "Teleco DaisyHub".lower()
 
+        self.installation: DaisyInstallation | None = None
         self.online = True
 
     def fetch_entities(self):
         self.lights = []
         self.covers = []
-        for installation in self.get_account_installation_list():
-            for room in self.get_room_list(installation):
+        installations = self.get_account_installation_list()
+        if installations:
+            self.installation = installations[0]  # nur die erste Installation
+            for room in self.get_room_list(self.installation):
                 for device in room.deviceList:
                     if isinstance(device, DaisyWhiteLight | DaisyRGBLight):
                         self.lights += [device]
                     elif isinstance(device, DaisyAwningsCover | DaisySlatsCover):
                         self.covers += [device]
 
+    def feed_command(self, command: dict):
+        if self.installation is None:
+            raise RuntimeError("Installation not loaded")
+        return self.feed_the_commands(self.installation, [command])
+
     @property
     def hub_id(self) -> str:
         return self._id
 
     async def test_connection(self) -> bool:
-        # TODO
+        # TODO: echte Prüfung
         return True
